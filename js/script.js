8846f959d05d1693d3a13ccc380e3c884eea3221
Ewa Czech <ewa.czech@hotmail.com>
2019-07-28 22:51:10 +0200

    add search functionality and mq to lightbox css

--- js/script.js
+++ js/script.js
@@ -1,6 +1,21 @@
-// $('.search__box').keypress(function(){
-//   console.log($('.search__box').val());
-// });
+let anchorElements = $('a');
+
+$('.search__box').keyup(function() {
+  $('a').hide();
+  let inputTypes = $('.search__box').val().toLowerCase();
+  if (inputTypes.length === 0) {
+    $('a').show();
+  } else {
+    anchorElements.each(function(i, element) {
+      let tempItem = element.dataset.title.toLowerCase();
+      if(tempItem.indexOf(inputTypes) > -1) {
+        $(element).show();
+      }
+    });
+  }
+});
+
+// Lightbox plugin option, code snippet taken from plugin documentation: https://lokeshdhakar.com/projects/lightbox2/
 
 lightbox.option({
       'alwaysShowNavOnTouchDevices': true,
