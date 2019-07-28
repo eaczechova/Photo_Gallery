253d3dec5d321cf718b1906f4ed12429521f8688
Ewa Czech <ewa.czech@hotmail.com>
2019-07-28 15:32:12 +0200

    add Lightbox plugin and adjust html stucture

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
+// $('.search__box').keypress(function(){
+//   console.log($('.search__box').val());
+// });
 
+lightbox.option({
+      'alwaysShowNavOnTouchDevices': true,
+      'resizeDuration': 200,
+      'wrapAround': true,
+      'maxWidth': 700,
+      'fitImagesInViewport': true
+    })
